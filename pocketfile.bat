0c369d4ef2709e67b43cc7d41a263b63d56f1339
Sherstiankin Nikita <nik.sherstyankin.75@mail.ru>
2024-05-22 20:36:37 +0300

    Add pocketfile.bat

---
+++ pocketfile.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\VSExpress.exe" project_test.sln /build Debug
+"C:\Program Files\doxygen\bin\doxygen.exe" config
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" setupProgMain.iss
